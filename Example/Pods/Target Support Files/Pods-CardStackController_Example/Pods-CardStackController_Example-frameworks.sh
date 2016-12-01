b94f5e0dbe86f2d89cfc4e042b2196e435c1622f
Victor Baro <fs.baro@gmail.com>
2016-11-30 16:36:24 -0800

    Remove reveal pod dependency

--- Example/Pods/Target Support Files/Pods-CardStackController_Example/Pods-CardStackController_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-CardStackController_Example/Pods-CardStackController_Example-frameworks.sh
@@ -85,9 +85,7 @@ strip_invalid_archs() {
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CardStackController/CardStackController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Reveal/Reveal.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/CardStackController/CardStackController.framework"
-  install_framework "$BUILT_PRODUCTS_DIR/Reveal/Reveal.framework"
 fi
--- Example/Pods/Target Support Files/Reveal/Reveal-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Reveal : NSObject
-@end
-@implementation PodsDummy_Reveal
-@end
